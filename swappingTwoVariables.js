cb23ef0a460cc0641b8ab9d323743eee47393e09
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2025-01-20 04:17:49 +0530

    Create swappingTwoVariables.js

---
+++ swappingTwoVariables.js
@@ -0,0 +1,23 @@
+// swapping two values
+
+var a = Number(prompt("Enter the first number : "))
+var b = Number(prompt("Enter the second number : "))
+
+// using the third variable
+
+// var c;
+
+// c = a
+// a = b
+// b = c
+
+// console.log(` a = ${a} 
+//               b = ${b}
+//             `)
+
+// without using the third variable
+
+            // a = 30  b = 20
+a = a + b  // a = 50 b = 20
+b = a - b  // a = 50  b = 30
+b = a - b  // a = 20  b = 30
