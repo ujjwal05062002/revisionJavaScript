ab09c365cc6c3615152076d43f6dab8da9d68b4e
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2025-01-22 07:25:10 +0530

    Create stringJS.js

---
+++ stringJS.js
@@ -0,0 +1,61 @@
+
+/*
+var a = "JavaScript is a scripting language."
+
+string in javascript
+
+    collection of character or sequence of character is known as String
+    strings are immutable
+    immutable means that it can not be modified later.
+
+string constant : 
+
+method 1:
+    'string'
+    "string"
+    `string
+    can be defined 
+    in this manner 
+    as well`
+
+string using loops :
+
+
+1. for loop
+
+for (let i=0; i<a.length; i++){
+    console.log(a[i])
+}
+
+2. while loop
+let i=0;
+while( i<a.length;){
+    console.log(a[i])
+    i++;
+}
+
+3. for of loop
+
+for (let i of a){
+console.log(i)}
+
+4. String Methods
+
+a. charAt
+
+
+
+console.log(a.charAt(1))
+
+b. charCodeAt
+
+console.log(a.charCodeAt(1));
+
+c. fromCharCode
+
+code = 66
+
+console.log(String.fromCharCode(66))
+
+*/
+
