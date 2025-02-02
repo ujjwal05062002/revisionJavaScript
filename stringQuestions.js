a2a91a456714096b824503b2203269f4daab7963
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2025-02-02 10:02:46 +0530

    Create stringQuestions.js

---
+++ stringQuestions.js
@@ -0,0 +1,12 @@
+// 1. Calculate occurence of lower case vowel, lower case consonants, upper case vowel, and upper case consonants in a string  
+
+
+
+// 2. count number of words in a string  
+
+// 3. count occurance of every character in a string
+
+
+// 4. delete repeated character from a string
+
+// 5. change first letter of every word of string 
