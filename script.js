32d4838bd1779eafd1c0bef608d8d9a8c06a26d0
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2025-05-26 07:06:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// wap to print sum of even and odd numbers in a range
+
+var start_num = prompt(Number("Enter the starting number : "))
+var end_num = prompt(Number("Enter the ending number : "))
+
+var even_sum = 0
+var odd_sum = 0
+
+for (var i = start_num; i<=end_num; i++){
+    
+}
